0731edd613dcb55554d500be10daaecc8be5fd2e
15mar <15mar@192.168.1.101>
2025-10-28 21:34:31 +0100

    carta + baraja

--- src/modelo/Baraja.java
+++ src/modelo/Baraja.java
@@ -1,5 +1,47 @@
 package modelo;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Baraja {
+	private List<Carta> baraja;
+	
+	public Baraja() {
+		this.baraja = new ArrayList<>();
+		for(int i=1; i <= 11; i++) {
+			baraja.add(new Carta(i));
+		}
+		Collections.shuffle(baraja);
+	}
+	
+	public Carta obtenerCarta() {
+		if(baraja.isEmpty()) {
+			return null;
+		}
+		return baraja.remove(0);
+	}
+	
+	public void devolverCarta(Carta c) {
+		baraja.add(c);
+		Collections.shuffle(baraja);
+	}
+	
+	public int cartasRestantes() {
+		return baraja.size();
+	}
+	
+	public boolean hayCartas() {
+		return !baraja.isEmpty();
+	}
+	
+	public void reiniciarBaraja() {
+		baraja.clear();
+		for(int i=1; i <= 11; i++) {
+			baraja.add(new Carta(i));
+		}
+		Collections.shuffle(baraja);
+	}
+	
 
 }
--- src/modelo/Carta.java
+++ src/modelo/Carta.java
@@ -1,5 +1,19 @@
 package modelo;
 
+//Clase para las cartas, las cartas únicamente tienen un valor entre 1 y 11
 public class Carta {
+	private int valor;
+	
+	public Carta(int valor) {
+		this.valor = valor;
+	}
+
+	public int getValor() {
+		return this.valor;
+	}
+	
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
 
 }
